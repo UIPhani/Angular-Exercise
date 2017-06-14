1c42aa6df6ba52824c632666640fa9cbc749527a
Venkat Phani Bhushan senaga setty <venkat@Venkats-MacBook-Pro.local>
2017-06-14 14:30:49 -0500

    final

--- app/app.js
+++ app/app.js
@@ -1,28 +1,15 @@
 var app = angular.module("UsersApp", []);
-
 app.controller("UsersCtrl", function($scope, $http) {
     $scope.hide_unselected=false;
     $scope.confirm_Active=true;
   $http.get('https://jsonplaceholder.typicode.com/users').
     then(function(data, status, headers, config) {
-          
            $scope.users = data.data;
-       //console.log( $scope.users);
     });
 
     $scope.showSelected=function (){
         $scope.hide_unselected=true;
-        var selected = [];
-        for( var i = 0; i < $scope.users.length; i++)
-        {
-            if($scope.users[i].checked)
-            {
-                selected.push($scope.users[i]);
-            }
-        }
-        console.log(selected);
     };
-
     $scope.totalUsersSelected= function (user){
           $scope.confirm_Active=false;
        if(user.checked === undefined)
